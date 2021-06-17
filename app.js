646686e8095baab577ce5390de7982f2cc59c40f
shahzadali57 <79161768+shahzadali57@users.noreply.github.com>
2021-06-17 21:42:50 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,80 @@
+// Object
+                            //Q1
+// var itemArray = [{
+//     name: 'juice',
+//     price: 50,
+//     quantity: 3
+// }, {
+//     name: 'cookie',
+//     price: 30,
+//     quantity: 9
+// }, {
+//     name: 'shirt',
+//     price: 880,
+//     quantity: 1
+// }, {
+//     name: 'pen',
+//     price: 100,
+//     quantity: 2
+// }];
+
+// var eachItem = itemArray[0].price + itemArray[1].price + itemArray[2].price + itemArray[3].price;
+// console.log(eachItem);
+// var forAllItem = (itemArray[0].price * itemArray[0].quantity) + (itemArray[1].price * itemArray[1].quantity) + (itemArray[2].price * itemArray[2].quantity) + (itemArray[3].price * itemArray[3].quantity);
+// console.log(forAllItem);
+
+                                // Q2
+// var q2Object = {
+//     name: 'Tanveer',
+//     email: 'tanveer.khan2692000@gmail.com',
+//     password: 'neverdie',
+//     age: '20',
+//     gender: 'Male',
+//     city: 'karachi',
+//     country: 'Pakistan'
+// }
+
+// var check1 = 'age' in q2Object;
+// var check2 = 'country' in q2Object;
+// var check3 = 'firstName' in q2Object;
+// var check4 = 'lastname' in q2Object;
+// console.log(check1, check2, check3, check4)
+
+                                       // Q3
+// function Const(name, fatherName, age, gender, city, country, district) {
+//     this.name = name;
+//     this.fatherName = fatherName;
+//     this.age = age;
+//     this.gender = gender;
+//     this.city = city;
+//     this.country = country;
+//     this.district = district;
+// }
+
+// var const1 = new Const('Tahir', 24, 'Male', 'Islamabad', 'Pakistan', 'North');
+// var const2 = new Const('Tanveer', 20, 'Male', 'Karachi', 'Pakistan', 'South');
+// var const3 = new Const('Anees', 20, 'Male', 'Gilgit', 'Pakistan', 'West');
+// var const4 = new Const('Shahraim', 19, 'Male', 'Faislabad', 'Pakistan', 'East');
+// var const5 = new Const('Mudassir', 21, 'Male', 'Lahore', 'Pakistan', 'North-East');
+// console.log(const1, const2, const3, const4, const5);
+
+
+                                    // Q4
+// function latest() {
+//     function Cunt(name, gender, address, education, profession) {
+//         this.name = name;
+//         this.gender = gender;
+//         this.address = address;
+//         this.education = education;
+//         this.profession = profession;
+//     }
+//     var n = document.getElementById('name1')
+//     var g = document.getElementsByClassName('gender')
+//     var a = document.getElementById('address')
+//     var e = document.getElementsByClassName('education')
+//     var p = document.getElementsByClassName('profession')
+//     var qw = new Cunt(n.value, g.innerHTML, a.innerHTML, e.value, p.value)
+//     console.log(qw)
+// }
+
+// shahzad ali
